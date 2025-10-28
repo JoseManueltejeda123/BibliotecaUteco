c226afc26a622d1a6dbba2c6f50edbb566acbc47
JoseManueltejeda123 <josemanueltejedalopez37@gmail.com>
2025-10-28 11:37:10 -0400

    bugs de validaciones resueltos

--- BibliotecaUteco/BibliotecaUteco.Client/Requests/Readers/Actions/UpdateReaderRequest.cs
+++ BibliotecaUteco/BibliotecaUteco.Client/Requests/Readers/Actions/UpdateReaderRequest.cs
@@ -14,8 +14,7 @@ public class UpdateReaderRequest
     public string FullName { get; set; } = null!;
 
     [Required(ErrorMessage = "El número de teléfono es obligatorio.")]
-    [MaxLength(10, ErrorMessage = "El número de teléfono debe tener exactamente 10 dígitos.")]
-    [MinLength(10, ErrorMessage = "El número de teléfono debe tener exactamente 10 dígitos.")]
+    [Length(10,10, ErrorMessage = "El número de teléfono debe tener exactamente 10 dígitos.")]
     public string PhoneNumber { get; set; } = null!;
 
     [Required(ErrorMessage = "La dirección es obligatoria.")]
@@ -24,8 +23,7 @@ public class UpdateReaderRequest
     public string Address { get; set; } = null!;
 
     [Required(ErrorMessage = "El número de cédula es obligatorio.")]
-    [MaxLength(11, ErrorMessage = "El número de cédula debe tener exactamente 11 dígitos.")]
-    [MinLength(11, ErrorMessage = "El número de cédula debe tener exactamente 11 dígitos.")]
+    [Length(11, 11, ErrorMessage = "El número de cédula debe tener exactamente 11 dígitos.")]
     public string IdentityCardNumber { get; set; } = null!;
 
     [Range(1, 2)]
